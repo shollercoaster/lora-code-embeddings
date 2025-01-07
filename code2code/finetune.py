eba6ddfd68973d9f00778531641112706dbd3237
shollercoaster <saumyachat03@gmail.com>
2025-01-07 14:13:13 +0100

    initial working scripts

---
+++ code2code/finetune.py
@@ -0,0 +1,69 @@
+from peft import LoraConfig, get_peft_model
+from transformers import AutoTokenizer, AutoModel, RobertaModel, RobertaTokenizer
+import sys
+from utils import get_dataset, collate_fn, ContrastiveTrainer
+from transformers import TrainingArguments
+
+def get_model(model_name):
+    model = RobertaModel.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = RobertaTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # named_modules/layers inside model
+    # for name, module in model.named_modules():
+        # print(name)
+    print("\n\nNamed Parameters\n")
+    for name, param in model.named_parameters():
+        print(name, param.data)
+
+    # print(model.config)
+    model = model.eval().cuda()
+    lora_config = LoraConfig(
+        r=64,
+        lora_alpha=128,
+        target_modules=['query', 'value'],
+        lora_dropout=0.1
+    )
+    
+    # model = get_peft_model(model, lora_config)
+    model.add_adapter(lora_config, adapter_name="code2code-r64")
+    model.set_adapter("code2code-r64")
+
+    # model.print_trainable_parameters()
+    return model, tokenizer
+
+
+languages = ["C", "PHP", "Java", "C++", "C#", "Javascript", "Python"]
+root_path = "../XLCoST_data"
+
+dataset = get_dataset(root_path=root_path, languages=languages)
+
+def run(model, tokenizer):
+    training_args = TrainingArguments(
+        "contrastive_trainer",
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        logging_steps=200,
+        num_train_epochs=1,
+        evaluation_strategy="no",
+        report_to="none",
+        remove_unused_columns=False,
+        warmup_steps=1000,
+        save_strategy="epoch"
+    )
+    trainer = ContrastiveTrainer(
+        model,
+        training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["val"],
+        data_collator=lambda x: collate_fn(x, tokenizer),
+    )
+    trainer.train()
+
+# print("active adapter before training: ", model.active_adapters())
+
+for model_name in ['microsoft/codebert-base', 'microsoft/graphcodebert-base', 'microsoft/unixcoder-base']:
+    model, tokenizer = get_model(model_name)
+    run(model, tokenizer)
+    print(f"\n\n Training completed with {model_name}. \n\n")
+    model.push_to_hub("code2code-r64")
---
+++ text2code/finetune.py
@@ -0,0 +1,67 @@
+import os
+from transformers import Trainer, TrainingArguments
+
+import torch
+from datasets import Dataset, DatasetDict
+import torch.nn.functional as F
+from transformers import AutoModel, AutoTokenizer, RobertaTokenizer, RobertaModel
+from torch.utils.data import DataLoader
+from peft import LoraConfig
+
+from utils import load_jsonl, CustomDataset, collate_fn, ContrastiveTrainer, _get_pooled_embeds
+
+languages = ['ruby', 'go', 'php', 'python', 'java', 'javascript']
+root_path = "../dataset/CSN"
+
+def get_dataset(root_path, languages, split):
+    for lang in languages:
+        data_path = os.path.join(root_path, lang, f"{split}.jsonl")
+        data_list = load_jsonl(data_path)
+
+    torch_dataset = CustomDataset(data_list)
+
+    return torch_dataset
+
+def get_model(model_name='microsoft/codebert-base'):
+    model = RobertaModel.from_pretrained(model_name)
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+    lora_config = LoraConfig(
+        r=64,
+        lora_alpha=128,
+        target_modules=['query', 'value'],
+        lora_dropout=0.1
+    )
+    
+    # model = get_peft_model(model, lora_config)
+    model.add_adapter(lora_config, adapter_name="text2code-r64")
+    model.set_adapter("text2code-r64")
+    return model, tokenizer
+
+def run(model, tokenizer):
+    training_args = TrainingArguments(
+        "contrastive_trainer",
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        logging_steps=400,
+        num_train_epochs=1,
+        evaluation_strategy="no",
+        report_to="none",
+        remove_unused_columns=False,
+        warmup_steps=4000,
+        save_strategy="epoch"
+    )
+    trainer = ContrastiveTrainer(
+        model,
+        training_args,
+        train_dataset=get_dataset(root_path=root_path, languages=languages, split="train"),
+        eval_dataset=get_dataset(root_path=root_path, languages=languages, split="valid"),
+        data_collator=lambda x: collate_fn(x, tokenizer),
+    )
+    trainer.train()
+
+for model_name in ['microsoft/codebert-base', 'microsoft/graphcodebert-base', 'microsoft/unixcoder-base']:
+    model, tokenizer = get_model(model_name)
+    run(model, tokenizer)
+    print(f"\n\n Training completed with {model_name}. \n\n")
+    model.push_to_hub("text2code-r64")
